cea3171496bf9155867f57a8384913203dd71dd6
Nick Bowers <greennick7777@gmail.com>
2017-07-11 02:01:35 -0400

    Adds section 3 and 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,9 +26,15 @@ function navHover() {
     if (distanceTop > 358 && distanceTop < 685) {
       $('nav ul li a').removeClass('navIndicator');
       $('nav ul li:nth-child(1) a').addClass('navIndicator');
-    } else if (distanceTop > 684) {
+    } else if (distanceTop > 684 && distanceTop < 1369) {
       $('nav ul li a').removeClass('navIndicator');
       $('nav ul li:nth-child(2) a').addClass('navIndicator');
+    } else if (distanceTop > 1368 && distanceTop < 1654) {
+      $('nav ul li a').removeClass('navIndicator');
+      $('nav ul li:nth-child(3) a').addClass('navIndicator');
+    } else if (distanceTop > 1653) {
+      $('nav ul li a').removeClass('navIndicator');
+      $('nav ul li:nth-child(4) a').addClass('navIndicator');
     } else {
       $('nav ul li a').removeClass('navIndicator');
     }
