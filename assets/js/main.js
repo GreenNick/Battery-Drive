4b33363066720476f7ff50d36c9ae5eebd0abf70
Nick Bowers <greennick7777@gmail.com>
2017-07-26 00:17:42 -0400

    adds responsive functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,39 +6,44 @@ $(document).ready(function() {
 function transformHeader() {
   $(window).scroll(function() {
     var distanceTop = $(window).scrollTop();
-
-    switch (true) {
-      case distanceTop > 358:
-        $('.hero-fixed').css({'display':'flex'});
-        $('.hero-fixed h1').fadeIn('fast');
+    if($(window).width() < 1000) {
+      switch (true) {
+      case distanceTop > $('.hero').innerHeight():
+        $('.hero-fixed').css({'height':'93px'});
         break;
       default:
-        $('.hero-fixed').css({'display':'none'});
-        $('.hero-fixed h1').hide('');
+        $('.hero-fixed').css({'height':'0'});
+      }
+    } else {
+      switch (true) {
+        case distanceTop > $('.hero').innerHeight() - 93:
+          $('.hero-fixed').css({'height':'93px'});
+          break;
+        default:
+          $('.hero-fixed').css({'height':'0'});
+      }
     }
   });
 }
 
 function navHover() {
   $(window).scroll(function() {
-    console.log($(window).scrollTop());
     var distanceTop = $(window).scrollTop();
-    console.log(distanceTop);
 
     switch(true) {
-      case distanceTop > 358 && distanceTop < 685:
+      case distanceTop > $('#section-1').offset().top - 130 && distanceTop < $('#section-2').offset().top - 130:
         $('nav ul li a').removeClass('navIndicator');
         $('nav ul li:nth-child(1) a').addClass('navIndicator');
         break;
-      case distanceTop > 684 && distanceTop < 1369:
+      case distanceTop > $('#section-2').offset().top - 130 && distanceTop < $('#section-3').offset().top - 130:
         $('nav ul li a').removeClass('navIndicator');
         $('nav ul li:nth-child(2) a').addClass('navIndicator');
         break;
-      case distanceTop > 1368 && distanceTop < 1654:
+      case distanceTop > $('#section-3').offset().top - 130 && distanceTop < $('#section-4').offset().top - 130:
         $('nav ul li a').removeClass('navIndicator');
         $('nav ul li:nth-child(3) a').addClass('navIndicator');
         break;
-      case distanceTop > 1653:
+      case distanceTop > $('#section-4').offset().top - 130:
         $('nav ul li a').removeClass('navIndicator');
         $('nav ul li:nth-child(4) a').addClass('navIndicator');
         break;
